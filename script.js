59afa8b8eb53fb8799f5ae4a0874a16b9e496e4d
izzywastaken <vasea.bajora@gmail.com>
2021-05-31 18:27:49 +0300

    Changed API Key

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 
-const APIKey = "73cf3798e2aaae0e83f979c21b88cf8a";
+const APIKey = "578f47eee8961c9e26c7e6ace6d5004a";
 const city = "London";
 //const URL = `api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`;
-const url = 'https://api.openweathermap.org/data/2.5/weather?q=London&appid=bd0c23ba43ed0ddab50f1ffd33aa9bdb';
+const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`;
+//http://openweathermap.org/img/wn/10d@2x.png
+
 
 $.getJSON(url, (data) => {
     console.log(data);
+    $("#date").text(data);
 })
 
 function init()
 {
-    $("#date").text();
+
 }
